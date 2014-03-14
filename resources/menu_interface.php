3a7ea9abe552410ec25a1bb587f67f92d9977a0c
Alex V. Surnin <info@volumnet.ru>
2014-03-14 21:01:16 +0500

    .

--- resources/menu_interface.php
+++ resources/menu_interface.php
@@ -5,7 +5,7 @@ $Item = new Menu(isset($config['menu']) ? (int)$config['menu'] : 0);
 if (!isset($config['full_menu']) || !(int)$config['full_menu']) {
     $Item = $Item->findPage($Page);
 }
-if ($Item->id) {
+if ($Item->id || $Item->page_id) {
     $OUT['Item'] = $Item;
 }
 return $OUT;
