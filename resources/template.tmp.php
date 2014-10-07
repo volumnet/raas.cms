4107a35932bec459543e25d1633a07c786d60ea0
Alex V. Surnin <info@volumnet.ru>
2014-10-07 19:53:11 +0600

    .

--- resources/template.tmp.php
+++ resources/template.tmp.php
@@ -9,60 +9,61 @@
   <body>
     <div class="background-holder">
       <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <header class="location_header container-fluid"><?php echo $Page->location('header')?></header>
-            <header class="location_menu_top"><?php echo $Page->location('menu_top')?></header>
-            <section class="main-container container-fluid">
-              <div class="row">
-                <main class="location_content col-sm-12">
+        <header class="location_header">
+          <div class="col-sm-5 location_header__logo"><?php echo $Page->locationBlocksText['header'][0]?></div>
+          <div class="col-sm-7 location_header__address"><?php echo $Page->locationBlocksText['header'][1]?></div> 
+        </header>
+        <header class="location_menu_top"><?php echo $Page->location('menu_top')?></header>
+        <section class="main-container row">
+          <?php if (count($Page->locationBlocksText['left'])) { ?>
+              <aside class="location_left col-sm-4"><?php echo $Page->location('left')?></aside>
+          <?php } ?>
+          <?php if (count($Page->locationBlocksText['center'])) { ?>
+              <section class="location_content col-sm-<?php echo (3 - (int)(bool)count($Page->locationBlocksText['left']) - (int)(bool)count($Page->locationBlocksText['right'])) * 4?>">
+                <?php 
+                if (!$Page->pid) { 
+                    echo $Page->locationBlocksText['content'][0];
+                }
+                ?>
+                <div class="block_content">
+                  <?php if ($Page->pid) { ?>
+                      <h1><?php echo htmlspecialchars($Page->name)?></h1>
+                  <?php } ?>
+                  <?php if ((count($Page->parents) + (bool)$Page->Material->id) > 1) { ?>
+                      <ol class="breadcrumb">
+                        <?php foreach ($Page->parents as $row) { ?>
+                            <li><a href="<?php echo htmlspecialchars($row->url)?>"><?php echo htmlspecialchars($row->name)?></a></li>
+                        <?php } ?>
+                        <?php if ($Page->Material->id) { ?>
+                            <li><a href="<?php echo htmlspecialchars($Page->url)?>"><?php echo htmlspecialchars($Page->oldName)?></a></li>
+                        <?php } ?>
+                      </ol>
+                  <?php } ?>
                   <?php 
-                  if (!$Page->pid) { 
-                      echo $Page->locationBlocksText['content'][0];
-                  }
+                  for ($i = (int)(!$Page->pid); $i < count($Page->locationBlocksText['content']); $i++) { 
+                      echo $Page->locationBlocksText['content'][$i];
+                  } 
                   ?>
-                  <div class="block_content">
-                    <?php if ($Page->pid) { ?>
-                        <h1><?php echo htmlspecialchars($Page->name)?></h1>
-                    <?php } ?>
-                    <?php if ((count($Page->parents) + (bool)$Page->Material->id) > 1) { ?>
-                        <ol class="breadcrumb">
-                          <?php foreach ($Page->parents as $row) { ?>
-                              <li><a href="<?php echo htmlspecialchars($row->url)?>"><?php echo htmlspecialchars($row->name)?></a></li>
-                          <?php } ?>
-                          <?php if ($Page->Material->id) { ?>
-                              <li><a href="<?php echo htmlspecialchars($Page->url)?>"><?php echo htmlspecialchars($Page->oldName)?></a></li>
-                          <?php } ?>
-                        </ol>
-                    <?php } ?>
-                    <?php 
-                    for ($i = (int)(!$Page->pid); $i < count($Page->locationBlocksText['content']); $i++) { 
-                        echo $Page->locationBlocksText['content'][$i];
-                    } 
-                    ?>
-                  </div>
-                </main>
-                <?php if (count($Page->locationBlocksText['left'])) { ?>
-                    <aside class="location_left col-sm-4"><?php echo $Page->location('left')?></aside>
-                <?php } ?>
-                <?php if (count($Page->locationBlocksText['center'])) { ?>
-                    <section class="location_center col-sm-<?php echo (3 - (int)(bool)count($Page->locationBlocksText['left']) - (int)(bool)count($Page->locationBlocksText['right'])) * 4?>">
-                      <?php echo $Page->location('center')?>
-                    </section> 
-                <?php } ?>
-                <?php if (count($Page->locationBlocksText['right'])) { ?>
-                    <aside class="location_left col-sm-4"><?php echo $Page->location('right')?></aside>
-                <?php } ?>
-              </div>
-            </section>
-            <footer class="location_footer container-fluid">
-              <div class="row location_footer__inner">
-                <?php echo $Page->location('footer')?>
-                <p class="developer">Разработка и сопровождение сайта <a href="http://volumnet.ru" target="_blank">Volume Networks</a></p>
-              </div>
-            </footer>
+                </div>
+              </section> 
+          <?php } ?>
+          <?php if (count($Page->locationBlocksText['right'])) { ?>
+              <aside class="location_right col-sm-4"><?php echo $Page->location('right')?></aside>
+          <?php } ?>
+        </section>
+        <footer class="location_footer">
+          <div class="location_footer__inner row">
+            <div class="col-sm-4"><?php echo $Page->locationBlocksText['footer'][0]?></div>
+            <div class="col-sm-8 clearfix">
+              <?php 
+              for ($i = 1; $i < count($Page->locationBlocksText['footer']); $i++) { 
+                  echo $Page->locationBlocksText['footer'][$i];
+              } 
+              ?>
+            </div>
+            <p class="developer">Разработка и сопровождение сайта <a href="http://volumnet.ru" target="_blank">Volume Networks</a></p>
           </div>
-        </div>
+        </footer>
       </div>
     </div>
     <?php echo $Page->location('footer_counters')?>
