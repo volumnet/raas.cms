ab5898e092e2800bd6f4efbafd4432727c9f5605
Alex V. Surnin <info@volumnet.ru>
2016-04-11 11:52:46 +0500

    .

--- resources/head.tmp.php
+++ resources/head.tmp.php
@@ -13,6 +13,9 @@
 <script src="/js/application.js"></script>
 <script src="/js/bootstrap.carousel.swipe.js"></script>
 <script src="/js/script.js"></script>
+<?php if (is_file('favicon.ico')) { ?>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 
+<?php } ?>
 <?php if (\SOME\HTTP::queryString()) { ?>
     <link rel="canonical" href="http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'] . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))?>">
 <?php } ?>
