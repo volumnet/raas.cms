5061fac3301831430a661882648748e23030964a
Alex V. Surnin <info@volumnet.ru>
2017-06-27 18:31:41 +0500

    .

--- resources/order_call_modal.tmp.php
+++ resources/order_call_modal.tmp.php
@@ -51,6 +51,15 @@ if ($_POST['AJAX'] && ($Item instanceof Feedback)) {
                         <button class="btn btn-primary" type="submit"><span class="fa fa-phone"></span></button>
                       </div>
                     </div>
+                    <?php $row = $Form->fields['agree'] ?>
+                    <div class="form-group">
+                      <div class="col-xs-12">
+                        <label class="checkbox">
+                          <?php $getField($row, $DATA);?>
+                          <a href="/privacy/" target="_blank"><?php echo htmlspecialchars($row->name)?></a>
+                        </label>
+                      </div>
+                    </div>
                     <?php if ($Form->antispam == 'captcha' && $Form->antispam_field_name) { ?>
                         <div class="form-group">
                           <label for="<?php echo htmlspecialchars($Form->antispam_field_name)?>" class="control-label col-sm-3"><?php echo CAPTCHA?></label>
