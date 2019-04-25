80a13e05bf46bf20f96d2c8b98e7483160b48b2e
Alex V. Surnin <info@volumnet.ru>
2019-04-25 15:12:06 +0500

    .

--- classes/catalog_cache.class.php
+++ classes/catalog_cache.class.php
@@ -1,17 +1,26 @@
 <?php
 namespace RAAS\CMS;
 
-use \RAAS\Attachment;
+use RAAS\Attachment;
 
 class Catalog_Cache
 {
     use CacheTrait;
 
-    protected $_mtype = array();
-    protected $_textKeys = array(
-        'urn', 'name', 'description', 'meta_title', 'meta_description', 'meta_keywords',
-        'h1', 'menu_name', 'breadcrumbs_name', 'article'
-    );
+    protected $_mtype = [];
+
+    protected $_textKeys = [
+        'urn',
+        'name',
+        'description',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        'h1',
+        'menu_name',
+        'breadcrumbs_name',
+        'article'
+    ];
 
     public function __get($var)
     {
@@ -26,9 +35,9 @@ class Catalog_Cache
     }
 
 
-    public function __construct(Material_Type $MType)
+    public function __construct(Material_Type $materialType)
     {
-        $this->_mtype = $MType;
+        $this->_mtype = $materialType;
     }
 
 
@@ -38,33 +47,32 @@ class Catalog_Cache
         $st = microtime(true);
         $this->clear();
 
-        $SQL_what = array();
+        $sqlWhat = [];
 
         // Категория непосредственно с товарами
-        $SQL_from = $SQL_where = array();
+        $sqlFrom = $sqlWhere = [];
         $sort = $order = "";
         if (!$this->_mtype->global_type) {
-            $SQL_from['tMPA'] = " LEFT JOIN " . Material::_dbprefix() . "cms_materials_pages_assoc AS tMPA ON tMPA.id = tM.id ";
+            $sqlFrom['tMPA'] = " LEFT JOIN " . Material::_dbprefix() . "cms_materials_pages_assoc AS tMPA ON tMPA.id = tM.id ";
         }
-        $SQL_where[] = " tM.vis ";
-        $types = array_merge(array((int)$this->_mtype->id), (array)$this->_mtype->all_children_ids);
-        $SQL_where[] = " tM.pid IN (" . implode(", ", $types) . ") ";
+        $sqlWhere[] = " tM.vis ";
+        $sqlWhere[] = " tM.pid IN (" . implode(", ", $this->_mtype->selfAndChildrenIds) . ") ";
 
         /*** FILTERING ***/
-        $SQL_array = array();
+        $sqlArray = [];
 
         // Набор полей
         if (!$this->_mtype->global_type) {
-            $SQL_what['pages_ids'] = "GROUP_CONCAT(DISTINCT tMPA.pid SEPARATOR '@@@') AS pages_ids";
+            $sqlWhat['pages_ids'] = "GROUP_CONCAT(DISTINCT tMPA.pid SEPARATOR '@@@') AS pages_ids";
         }
-        foreach (array_merge(array($this->_mtype), (array)$this->_mtype->children) as $mtype) {
+        foreach ($this->_mtype->selfAndChildren as $mtype) {
             foreach ($mtype->selfFields as $row) {
-                if (in_array($row->urn, array('price', 'price_old'))) {
+                if (in_array($row->urn, ['price', 'price_old'])) {
                     $temp = "CAST(value AS UNSIGNED)";
                 } else {
                     $temp = "value";
                 }
-                $SQL_what[$row->urn] = "(
+                $sqlWhat[$row->urn] = "(
                     SELECT GROUP_CONCAT(DISTINCT " . $temp . " ORDER BY fii ASC SEPARATOR '@@@')
                       FROM " . Field::data_table . "
                      WHERE pid = tM.id
@@ -76,13 +84,13 @@ class Catalog_Cache
         /*** QUERY ***/
         Material::_SQL()->query("SET SESSION group_concat_max_len=1000000");
         Material::_SQL()->query("SET SQL_BIG_SELECTS=1");
-        $SQL_query = "SELECT SQL_CALC_FOUND_ROWS tM.* " . ($SQL_what ? ", " . implode(", ", $SQL_what) : "")
-                   . "  FROM " . Material::_tablename() . " AS tM " . implode(" ", $SQL_from)
-                   . ($SQL_where ? " WHERE " . implode(" AND ", $SQL_where) : "")
+        $sqlQuery = "SELECT SQL_CALC_FOUND_ROWS tM.* " . ($sqlWhat ? ", " . implode(", ", $sqlWhat) : "")
+                   . "  FROM " . Material::_tablename() . " AS tM " . implode(" ", $sqlFrom)
+                   . ($sqlWhere ? " WHERE " . implode(" AND ", $sqlWhere) : "")
                    . " GROUP BY tM.id ORDER BY NOT tM.priority, tM.priority ASC ";
-        // echo $SQL_query; exit;
-        $SQL_result = Material::_SQL()->get($SQL_query);
-        $SQL_result = array_map(
+        // echo $sqlQuery; exit;
+        $sqlResult = Material::_SQL()->get($sqlQuery);
+        $sqlResult = array_map(
             function ($x) use ($t) {
                 $y = $x;
                 foreach ($y as $key => $val) {
@@ -98,12 +106,12 @@ class Catalog_Cache
                 }
                 return $y;
             },
-            $SQL_result
+            $sqlResult
         );
 
-        // print_r ($SQL_result); exit;
+        // print_r ($sqlResult); exit;
 
-        $this->data = $SQL_result;
+        $this->data = $sqlResult;
         $this->save();
     }
 
@@ -117,7 +125,7 @@ class Catalog_Cache
     public function checkDeepNumeric($data, $key = null)
     {
         if (is_array($data)) {
-            $temp = array();
+            $temp = [];
             foreach ($data as $key => $val) {
                 $temp[$key] = $this->checkDeepNumeric($val, $key);
             }
