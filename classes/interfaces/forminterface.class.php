6dd95f1f9b0306735e33e766c4ca46ee06d45b64
Alex V. Surnin <info@volumnet.ru>
2019-09-12 18:24:58 +0500

    .

--- classes/interfaces/forminterface.class.php
+++ classes/interfaces/forminterface.class.php
@@ -16,41 +16,6 @@ use RAAS\View_Web as RAASViewWeb;
  */
 class FormInterface extends AbstractInterface
 {
-    /**
-     * Конструктор класса
-     * @param Block_Form|null $block Блок, для которого применяется
-     *                               интерфейс
-     * @param Page|null $page Страница, для которой применяется интерфейс
-     * @param array $get Поля $_GET параметров
-     * @param array $post Поля $_POST параметров
-     * @param array $cookie Поля $_COOKIE параметров
-     * @param array $session Поля $_SESSION параметров
-     * @param array $server Поля $_SERVER параметров
-     * @param array $files Поля $_FILES параметров
-     */
-    public function __construct(
-        Block_Form $block = null,
-        Page $page = null,
-        array $get = [],
-        array $post = [],
-        array $cookie = [],
-        array $session = [],
-        array $server = [],
-        array $files = []
-    ) {
-        parent::__construct(
-            $block,
-            $page,
-            $get,
-            $post,
-            $cookie,
-            $session,
-            $server,
-            $files
-        );
-    }
-
-
     public function process()
     {
         $result = [];
